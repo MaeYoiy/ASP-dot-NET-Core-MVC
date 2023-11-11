bfcfeef5a7ae62c643a72650885d749faf159139
MaeYoiy <b6333034@gmail.com>
2023-11-11 20:09:32 +0700

    create Model and View

--- BasicASPTutorial/BasicASPTutorial/Controllers/StudentController.cs
+++ BasicASPTutorial/BasicASPTutorial/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BasicASPTutorial.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BasicASPTutorial.Controllers
 {
@@ -6,7 +7,33 @@ namespace BasicASPTutorial.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            //รูปแบบที่ 1
+            Student s1 = new Student();
+            //รูปแบบที่ 2  (นิยมที่สุด)
+            //var s2 = new Student();
+            //รูปแบบที่ 3
+            //Student s3 = new();
+
+            s1.Id = 1;
+            s1.Name = "Mink";
+            s1.Score = 100;
+
+            var s2 = new Student();
+            s2.Id = 2;
+            s2.Name = "Watchapol";
+            s2.Score = 40;
+
+            Student s3 = new();
+            s3.Id = 3;
+            s3.Name = "Yeen";
+            s3.Score = 90;
+
+            List<Student> allStudent = new List<Student>();
+            allStudent.Add(s1);
+            allStudent.Add(s2);
+            allStudent.Add(s3);
+
+            return View(allStudent);
 
         }
 
---
+++ BasicASPTutorial/BasicASPTutorial/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicASPTutorial.Models
+{
+    public class Student
+    {
+        //Create Model
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+    }
+}
